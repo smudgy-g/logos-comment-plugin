e9d5c2a4f1dfa2bcf2ac6599049ebe633d80e82c
smudgy-g <adamgriff86@gmail.com>
2023-12-23 14:36:04 +0100

    feat: comment Component

---
+++ src/components/CommentComponent/CommentComponent.tsx
@@ -0,0 +1,32 @@
+
+import { Card, CardBody } from '@acid-info/lsd-react'
+import { useCommentContext } from '../../context/CommentsContext/comments'
+import CommentForm from '../CommentForm'
+import CommentCard from '../CommentCard'
+
+const CommentComponent = () => {
+  const { comments } = useCommentContext()
+
+  return (
+    <>
+      <CommentForm />
+      <div className="space-y-3">
+        {comments ? (
+          comments.map((comment) => (
+            <CommentCard  data={comment} key={comment.comment_id}/>
+          ))
+        ) : (
+          <>
+            <Card>
+              <CardBody>
+                <p>No comments yet. Be the first to comment.</p>
+              </CardBody>
+            </Card>
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default CommentComponent
