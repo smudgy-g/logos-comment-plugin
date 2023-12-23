a03122888a6ab7d715a81ea2dd24b344f50f5239
smudgy-g <adamgriff86@gmail.com>
2023-12-23 14:49:23 +0100

    package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { AuthProvider } from './context/auth.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-)
+export * from './components';
