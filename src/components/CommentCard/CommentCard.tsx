91d4e9c838dc2a3c0f527c38bcdbffaf63d3ee80
smudgy-g <adamgriff86@gmail.com>
2023-12-23 14:35:46 +0100

    feat: comment card

---
+++ src/components/CommentCard/CommentCard.tsx
@@ -0,0 +1,32 @@
+import { formatTimestamp } from '../../utils/timestamp'
+import { Comment, useCommentContext } from '../../context/CommentsContext/comments'
+import { Button, ButtonGroup, Card, CardBody, CardHeader } from '@acid-info/lsd-react'
+
+const CommentCard = ({ data}: { data: Comment }) => {
+  const { updateLikes } = useCommentContext()
+  const { comment_id, content, created_at, likes, username } = data
+
+  return (
+    <Card size={"small"}>
+      <CardHeader>
+        <div className="flex justify-between w-full">
+          <span>{username}</span>
+          <span className="text-xs">{formatTimestamp(created_at)}</span>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <p>{content}</p>
+
+        
+        <div className="flex justify-between items-end">
+          <p className="text-xs">{likes} Likes</p>
+          <ButtonGroup>
+            <Button variant={"outlined"} size={"small"} onClick={() => updateLikes(comment_id, likes)}>Like</Button>
+          </ButtonGroup>
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default CommentCard
